bab4da20e482ba4710b7723c93930b09ee659013
Christopher Harrison <ch12@sanger.ac.uk>
2018-08-20 16:08:13 +0100

    Sort-of-working last updated component

---
+++ src/components/LastUpdated.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :title="updateTime">Last updated {{ fuzzyDuration() }}.</div>
+</template>
+
+<script>
+let timer = null
+
+export default {
+  name: 'LastUpdated',
+  props: [ 'when' ],
+  data () {
+    return { now: Date.now() }
+  },
+  computed: {
+    updateTime () {
+      return Date(this.when)
+    }
+  },
+  methods: {
+    fuzzyDuration () {
+      if (isNaN(this.when)) {
+        return '...'
+      }
+
+      // Duration in minutes
+      var delta = (this.now - this.when) / 60000
+
+      if (delta < 1) {
+        return 'just now'
+      } else {
+        var minutes = Math.floor(delta)
+        return `${minutes} minute` +
+               (minutes === 1 ? '' : 's') +
+               ' ago'
+      }
+    }
+  },
+  created () {
+    timer = setInterval(() => { this.now = Date.now() }, 1000)
+  },
+  destroyed () {
+    clearInterval(timer)
+  }
+}
+</script>
